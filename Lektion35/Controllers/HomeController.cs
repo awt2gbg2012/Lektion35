d8eb9f12c2cb429728442bc8c7eb6d07603ffb87
David Gullmarsvik <awt2.2012@gmail.com>
2012-11-06 08:29:40 +0100

    Breaking Change 1

--- Lektion35/Controllers/HomeController.cs
+++ Lektion35/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace Lektion35.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Welcome to ASP.NET MVC!";
+            ViewBag.Message = "Welcome to ASP__NET MVC!";
 
             return View();
         }
